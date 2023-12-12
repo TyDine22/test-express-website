eb881e19c58e90525cfae21e12378fd6deec7f74
TyDine22 <150012901+TyDine22@users.noreply.github.com>
2023-12-12 09:55:49 +0000

    implement mustache and and job lists for mobile

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const path = require('path');
+const JOBS = require('./jobs');
+const mustacheExpress = require('mustache-express');
+
+console.log(JOBS);
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+//think of views as template
+app.set('views', path.join(__dirname, 'pages'));
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+
 app.get('/', (req, res) => {
     // res.send('Hello Mista!');
-    res.sendFile(path.join(__dirname, 'pages/index.html'));
+    // res.sendFile(path.join(__dirname, 'pages/index.html'));
+    res.render('index', { jobs : JOBS});
 });
 
 const port = process.env.PORT || 3000;
---
+++ src/jobs.js
@@ -0,0 +1,39 @@
+const JOBS = [
+    {
+        id: 1,
+        title: 'Frontend Developer',
+        location: 'Phnom Penh, Cambodia',
+        salary: '26000000៛',
+        posted: 'March 26, 2023',
+        requirement: 'Degree in Computer Science, HTML & CSS'
+    },
+
+    {
+        id: 2,
+        title: 'Backend Developer',
+        location: 'Remote',
+        salary: '29000000៛',
+        posted: 'Feb 16, 2023',
+        requirement: 'Degree in Computer Science, Database, Server'
+    },
+
+    {
+        id: 3,
+        title: 'Data scientist',
+        location: 'Remote',
+        salary: '$150000',
+        posted: 'April 13, 2023',
+        requirement: 'Degree in Computer Science, Data Structure and Algorithms'
+    },
+
+    {
+        id: 4,
+        title: 'Data Analyse',
+        location: 'Phnom Penh, Cambodia',
+        salary: '$64000',
+        posted: 'April 22, 2023',
+        requirement: 'Degree in Computer Science, Data Structure and Algorithms'
+    }
+]
+
+module.exports = JOBS;
